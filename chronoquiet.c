d55b0b15607d6b919b6f380f94870c78486d23e9
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2023-03-09 18:17:05 +0000

    cleanup code

--- chronoquiet.c
+++ chronoquiet.c
@@ -8,70 +8,66 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   struct timeval t1, t2;
   uint64_t ut1, ut2;
   int ret, i, child_ret;
   char **exec_line;
   pid_t pid;
   
-  if(argc < 2)
-  {
+  if(argc < 2) {
     fprintf(stderr, "Usage : %s <command>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
   
   /* Create array for execv */
   exec_line = (char **)malloc((argc) * sizeof(char *));
-  for(i=1; i<argc; i++)
-  {
-	exec_line[i-1] = (char *)malloc((strlen(argv[i]) + 1) * sizeof(char));
-	strcpy(exec_line[i-1], argv[i]);
+  for(i=1; i<argc; i++) {
+	  exec_line[i-1] = (char *)malloc((strlen(argv[i]) + 1) * sizeof(char));
+	  strcpy(exec_line[i-1], argv[i]);
   }
   exec_line[argc - 1] = NULL;
     
-  if(gettimeofday(&t1, NULL))
-  {
+  if(gettimeofday(&t1, NULL)) {
     perror("gettimeofday");
     goto err_free;
   }
   
   pid = vfork();
-  if(pid == -1)
-  {
-	perror("vfork");
-	goto err_free;
-  }
-  else if(pid == 0)
-  {
-	freopen("/dev/null", "w", stderr);
-	freopen("/dev/null", "w", stdout);
+  if(pid == -1) {
+
+    /* error vfork */
+	  perror("vfork");
+	  goto err_free;
+  } else if(pid == 0) {
+
+    /* child, run command */
+	  freopen("/dev/null", "w", stderr);
+	  freopen("/dev/null", "w", stdout);
     execvp(exec_line[0], exec_line);
+
+    /* shouldn't reach here */
     perror("child exec");
-	exit(EXIT_FAILURE);
-  }
-  else
-  { 
+	  exit(EXIT_FAILURE);
+  } else { 
+
+    /* parent, wait for child */
     ret = waitpid(pid, &child_ret, 0);
     
-    if(gettimeofday(&t2, NULL))
-    {
+    if(gettimeofday(&t2, NULL)) {
       perror("gettimeofday");
       goto err_free;
     }
     
-    if(ret == -1)
-    {
-		perror("waitpid");
-		goto err_free;
-	}
+    if(ret == -1) {
+		  perror("waitpid");
+		  goto err_free;
+	  }
 	
-    if(child_ret)
-    {
-        fprintf(stderr, "Error: child returned non zero exit status: %d\n", 
+    if(child_ret) {
+      fprintf(stderr, "Error: child returned non zero exit status: %d\n", 
                 WEXITSTATUS(child_ret));
-        goto err_free;           
+      goto err_free;           
     }
   }
    
@@ -88,7 +84,7 @@ int main(int argc, char **argv)
 
 err_free:
   for(i=0; i<(argc-1); i++)
-	free(exec_line[i]);
+	  free(exec_line[i]);
   free(exec_line);
   return EXIT_FAILURE;
 }
